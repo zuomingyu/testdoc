1a3111d1000c16e82b22f897b3a158a84f988593
Xiaowu Chen <five3@163.com>
2013-05-21 18:04:30 +0800

    fix redirect url wrong in nginx

--- config/url.py
+++ config/url.py
@@ -6,10 +6,10 @@ pre_fix = 'controllers.'
 ##URL匹配可以使用正则
 urls = (
         
-    '^/$',                               'redirect /seleniumdoc/index', 
-    '^/seleniumdoc/$',                               'redirect /seleniumdoc/index', 
+    '^/$',                               'redirect seleniumdoc/index', 
+    '^/seleniumdoc/$',                               'redirect seleniumdoc/index', 
     '^/seleniumdoc/([^/]+)$',                        pre_fix + 'selenium.index',  
-    '^/pythonapi/$',                               'redirect /pythonapi/index',  
+    '^/pythonapi/$',                               'redirect pythonapi/index',  
     '^/pythonapi/([^/]+)$',                        pre_fix + 'pythonapi.%s', 
     
 )
